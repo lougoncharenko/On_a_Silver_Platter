4590088384ef9c25bc0f0234f8b085ff48b0518b
Louisa Goncharenko <louisa.goncharenko@shopify.com>
2022-10-06 09:52:15 -0400

    added a section to test apple class for encapsulation

--- tests/encapsulation.test.js
+++ tests/encapsulation.test.js
@@ -1,5 +1,6 @@
 //import classes
 const Fruit = require('../app/fruit');
+const Apple = require('../app/apple');
 
 //Tests fruit.js
 describe('Fruit', () => {
@@ -21,9 +22,50 @@ describe('Fruit', () => {
 
         expect(() => orange.setFruit(50)).toThrow('50 is not a valid string');
       });
-
-
 });
+
+//tests Apple.js
+describe('Apple', () => {
+    const consoleSpy = jest.spyOn(console, 'log');
+  
+    afterEach(() => {
+      consoleSpy.mockClear();
+    });
+
+    test('setAmmount will return a number if number is not a decimal or negative number', () => {
+        const apple = new Apple();
+        apple.setAmmount(50);
+
+        expect(apple.getAmmount()).toBe(50);
+    })
+
+
+    test('#setAmmount logs an error message if a whole number is not given', () => {
+        const apple = new Apple();
+
+        expect(() => apple.setAmmount(10.6)).toThrow('10.6 is not a whole number greater than zero');
+        
+    });
+
+    test('#setAmmount logs an error message if a whole number is not given', () => {
+        const apple = new Apple();
+
+        expect(() => apple.setAmmount(-7)).toThrow('-7 is not a whole number greater than zero');
+        
+    });
+});
+
+// describe('Book', () => {
+//     const consoleSpy = jest.spyOn(console, 'log');
+  
+//     afterEach(() => {
+//       consoleSpy.mockClear();
+//     });
+
+// test('', () => {
+
+// })
+// });
 
 //blank copy for tests
 // describe('Book', () => {
