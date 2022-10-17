0dd914f3b6a5b546a87bc31992508b6771b8e1c8
Louisa Goncharenko <louisa.goncharenko@shopify.com>
2022-10-17 09:44:55 -0400

    added a section to test cheese.js

--- tests/inheritsnce.test.js
+++ tests/inheritsnce.test.js
@@ -1,5 +1,6 @@
 //import class
 const Apple = require('../app/apple');
+const Banana = require('../app/banana');
 
 describe('Apple', () => {
     const consoleSpy = jest.spyOn(console, 'log');
@@ -25,3 +26,32 @@ describe('Apple', () => {
         expect(apple.eat()).toBe('Customer bought 30 apples to eat')
     })
 });
+
+
+describe('Banana', () => {
+    const consoleSpy = jest.spyOn(console, 'log');
+  
+    afterEach(() => {
+      consoleSpy.mockClear();
+    });
+
+    test('Banana inherits fruits toString(), setFruit(), getFruit() methods', () => {
+        const banana = new Banana();
+        banana.setFruit('banana');
+        banana.getFruit();
+        
+        expect(banana.toString()).toBe('banana is tasty!')
+
+    })
+
+    test('Banana inherits eat() method', () => {
+        const banana = new Banana();
+        banana.setFruit('banana');
+        banana.getFruit();
+        
+        expect(banana.eat()).toBe('You eat a banana')
+
+    })
+});
+
+
